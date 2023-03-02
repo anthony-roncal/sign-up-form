d9d648b384bf97aeac8a787b77e7ecdb10e5d2f9
Anthony <acr1191@gmail.com>
2023-03-02 12:08:50 -0800

    add regex formatting for email. also requires numerical input for phone number, and formats phone number as it is being entered

--- script.js
+++ script.js
@@ -1,24 +1,83 @@
-const form = document.getElementById("form");
-const passwordField = document.getElementById("password");
-const password2Field = document.getElementById("password2");
-const errorMessage = document.querySelector("span.error");
+const form = document.getElementById('form');
+const formFirstName = document.getElementById('first-name');
+const errorFirstName = formFirstName.nextElementSibling;
+const formLastName = document.getElementById('last-name');
+const errorLastName = formLastName.nextElementSibling;
+const formEmail = document.getElementById('email');
+const errorEmail = formEmail.nextElementSibling;
+const formPhone = document.getElementById('phone');
+const errorPhone = formPhone.nextElementSibling;
+const formCountry = document.getElementById('country');
+const errorCountry = formCountry.nextElementSibling;
+const formZip = document.getElementById('zip');
+const errorZip = formZip.nextElementSibling;
+const formPassword = document.getElementById('password');
+const errorPassword = formPassword.nextElementSibling;
+const formPassword2 = document.getElementById('password2');
+const errorPassword2 = formPassword2.nextElementSibling;
 
-passwordField.addEventListener('input', comparePasswords);
-password2Field.addEventListener('input', comparePasswords);
-form.addEventListener('submit', comparePasswords);
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-function comparePasswords(e) {
-    if (passwordField.value !== password2Field.value) {
-        console.log(`passwordField.value = ${passwordField.value}`);
-        console.log(`password2Field.value = ${password2Field.value}`);
-        passwordField.classList.add('error');
-        password2Field.classList.add('error');
-        errorMessage.textContent = "Passwords do not match";
-    } else if (passwordField.value === password2Field.value) {
-        console.log(`passwordField.value = ${passwordField.value}`);
-        console.log(`password2Field.value = ${password2Field.value}`);
-        passwordField.classList.remove('error');
-        password2Field.classList.remove('error');
-        errorMessage.textContent = "";
+formFirstName.addEventListener('input', isFieldEmpty);
+formLastName.addEventListener('input', isFieldEmpty);
+formEmail.addEventListener('input', isFieldEmpty);
+formPhone.addEventListener('input', isFieldEmpty);
+formCountry.addEventListener('input', isFieldEmpty);
+formZip.addEventListener('input', isFieldEmpty);
+formPassword.addEventListener('input', isFieldEmpty);
+formPassword2.addEventListener('input', isFieldEmpty);
+
+function isFieldEmpty(e) {
+    if(e.target.value.length === 0) {
+        e.target.className = 'invalid';
+        e.target.nextElementSibling.textContent = `${e.target.dataset.name} is required`;
+    } else {
+        e.target.className = 'valid';
+        e.target.nextElementSibling.textContent = '';
     }
 }
+
+formEmail.addEventListener('input', () => {
+    if(formEmail.value.length > 0 && !emailRegex.test(formEmail.value)) {
+        formEmail.className = 'invalid';
+        errorEmail.textContent = 'Please enter a valid email';
+    }
+});
+
+formPhone.addEventListener('input', (e) => {
+    var phoneNumber = formPhone.value.replace(/[^\d]/g, '');
+    formPhone.value = phoneNumber;
+    if(phoneNumber.length < 4) formPhone.value = phoneNumber;
+    if(phoneNumber.length >= 4 && phoneNumber.length < 7) {
+        formPhone.value = `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3)}`;
+    }
+    if(phoneNumber.length >= 7) {
+        formPhone.value = `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3,6)}-${phoneNumber.slice(6,10)}`;
+    }
+    if(phoneNumber.length < 10) {
+        formPhone.className = 'invalid';
+        errorPhone.textContent = 'Please enter a valid 10-digit phone number';
+    }
+});
+
+// formPassword.addEventListener('input', comparePasswords);
+// formPassword2.addEventListener('input', comparePasswords);
+
+// function comparePasswords(e) {
+//     if (formPassword.value !== formPassword2.value) {
+//         console.log(`formPassword.value = ${formPassword.value}`);
+//         console.log(`formPassword2.value = ${formPassword2.value}`);
+//         formPassword.className('invalid');
+//         formPassword2.className('invalid');
+//     } else if (formPassword.value === formPassword2.value) {
+//         console.log(`formPassword.value = ${formPassword.value}`);
+//         console.log(`formPassword2.value = ${formPassword2.value}`);
+//         formPassword.className('valid');
+//         formPassword2.className('valid');
+//     }
+// }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('submit');
+});
