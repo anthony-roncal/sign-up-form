f1e2473ee0b9b8ff449cc85eef3229c7c3be7ae1
Anthony <acr1191@gmail.com>
2023-03-02 17:03:44 -0800

    change country field to select. add validation for zip based on selected country. updated confirm password validation

--- script.js
+++ script.js
@@ -15,17 +15,28 @@ const formPassword = document.getElementById('password');
 const errorPassword = formPassword.nextElementSibling;
 const formPassword2 = document.getElementById('password2');
 const errorPassword2 = formPassword2.nextElementSibling;
+const formSubmitBtn = form.querySelector('form > button');
+const errorSubmitBtn = formSubmitBtn.nextElementSibling;
 
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-formFirstName.addEventListener('input', isFieldEmpty);
-formLastName.addEventListener('input', isFieldEmpty);
-formEmail.addEventListener('input', isFieldEmpty);
-formPhone.addEventListener('input', isFieldEmpty);
-formCountry.addEventListener('input', isFieldEmpty);
-formZip.addEventListener('input', isFieldEmpty);
-formPassword.addEventListener('input', isFieldEmpty);
-formPassword2.addEventListener('input', isFieldEmpty);
+const countryRegex = [
+    {country: "US", regex: /^\d{5}([\-]?\d{4})?$/},
+    {country: "UK", regex: /^(GIR|[A-Z]\d[A-Z\d]??|[A-Z]{2}\d[A-Z\d]??)[ ]??(\d[A-Z]{2})$/},
+    {country: "DE", regex: /^\d{5}$/},
+    {country: "CA", regex: /^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$/},
+    {country: "FR", regex: /^(F-)?((2[A|B])|[0-9]{2})[0-9]{3}$/},
+    {country: "IT", regex: /^(V-|I-)?[0-9]{5}$/},
+    {country: "AU", regex: /^(0[289][0-9]{2})|([1345689][0-9]{3})|(2[0-8][0-9]{2})|(290[0-9])|(291[0-4])|(7[0-4][0-9]{2})|(7[8-9][0-9]{2})$/},
+    {country: "NL", regex: /^[1-9][0-9]{3}\s?([a-zA-Z]{2})?$/},
+    {country: "ES", regex: /^([1-9]{2}|[0-9][1-9]|[1-9][0-9])[0-9]{3}$/},
+    {country: "DK", regex: /^([D|d][K|k]( |-))?[1-9]{1}[0-9]{3}$/},
+    {country: "SE", regex: /^(s-|S-){0,1}[0-9]{3}\s?[0-9]{2}$/},
+    {country: "BE", regex: /^[1-9]{1}[0-9]{3}$/},
+    {country: "IN", regex: /^\d{6}$/}
+];
+
+document.querySelectorAll('.form-item > input').forEach(input => input.addEventListener('input', isFieldEmpty));
 
 function isFieldEmpty(e) {
     if(e.target.value.length === 0) {
@@ -54,30 +65,68 @@ formPhone.addEventListener('input', (e) => {
     if(phoneNumber.length >= 7) {
         formPhone.value = `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3,6)}-${phoneNumber.slice(6,10)}`;
     }
-    if(phoneNumber.length < 10) {
+    if(phoneNumber.length > 0 && phoneNumber.length < 10) {
         formPhone.className = 'invalid';
         errorPhone.textContent = 'Please enter a valid 10-digit phone number';
     }
 });
 
-// formPassword.addEventListener('input', comparePasswords);
-// formPassword2.addEventListener('input', comparePasswords);
+countryRegex.forEach(item => {
+    var option = document.createElement('option');
+    option.value = item.country;
+    option.textContent = item.country;
+    formCountry.appendChild(option);
+});
 
-// function comparePasswords(e) {
-//     if (formPassword.value !== formPassword2.value) {
-//         console.log(`formPassword.value = ${formPassword.value}`);
-//         console.log(`formPassword2.value = ${formPassword2.value}`);
-//         formPassword.className('invalid');
-//         formPassword2.className('invalid');
-//     } else if (formPassword.value === formPassword2.value) {
-//         console.log(`formPassword.value = ${formPassword.value}`);
-//         console.log(`formPassword2.value = ${formPassword2.value}`);
-//         formPassword.className('valid');
-//         formPassword2.className('valid');
-//     }
-// }
+formCountry.addEventListener('change', () => formZip.value = '');
+
+formZip.addEventListener('input', () => {
+    var zipRegex = countryRegex.find(item => item.country === formCountry.value).regex;
+    if(formZip.value.length > 0 && !zipRegex.test(formZip.value)) {
+        formZip.className = 'invalid';
+        errorZip.textContent = 'Please enter a valid zip code';
+    }
+});
+
+formPassword.addEventListener('input', comparePasswords);
+formPassword2.addEventListener('input', comparePasswords);
+
+function comparePasswords(e) {
+    if(formPassword.value.length > 0 && formPassword2.value.length > 0) {
+        if(formPassword.value !== formPassword2.value) {
+            console.log(`formPassword.value = ${formPassword.value}`);
+            console.log(`formPassword2.value = ${formPassword2.value}`);
+            formPassword.className = 'invalid';
+            formPassword2.className = 'invalid';
+            errorPassword2.textContent = 'Passwords do not match';
+        } else if (formPassword.value === formPassword2.value) {
+            console.log(`formPassword.value = ${formPassword.value}`);
+            console.log(`formPassword2.value = ${formPassword2.value}`);
+            formPassword.className = 'valid';
+            formPassword2.className = 'valid';
+            errorPassword2.textContent = '';
+        }
+    }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('submit');
+
+    let errorMessage = 'Please check the following fields: ';
+    let invalidFields = [];
+    document.querySelectorAll('.form-item > input').forEach(input => {
+        if(!input.className) {
+            input.className = 'invalid';
+        }
+        if(input.className === 'invalid') {
+            invalidFields.push(input.dataset.name);
+        }
+    });
+    if(invalidFields.length === 0) {
+        errorSubmitBtn.textContent = '';
+    } else {
+        errorMessage += invalidFields.join(', ');
+        errorSubmitBtn.textContent = errorMessage;
+    }
 });
+
